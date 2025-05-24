5e9c5137da4e90cbd78ffaf277a99939bf8ee50e
Amber Afreen <103463636+sanakhan8859@users.noreply.github.com>
2025-05-24 22:37:21 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -42,7 +42,7 @@ const projectsData: Project[] = [
     image:
       "https://images.unsplash.com/photo-1517180102446-f3ece451e9d8?w=600&q=80",
     github: "https://github.com/sanakhan8859/Portfolio.git",
-    demo: "https://portfolio.example.com",
+    demo: "https://portfolio-orcin-theta-57.vercel.app/",
     category: "Web Development",
   },
   {
